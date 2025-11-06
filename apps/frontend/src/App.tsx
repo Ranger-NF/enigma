3cdc3e1532d85aaa9ed8d053ef84e8e1d5a03eea
sooryanathg <sooryanathgopi@gmail.com>
2025-11-06 09:46:43 +0530

    clean up of pr 21

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,18 +1,7 @@
-import { ThemeProvider } from "./components/theme-provider";
-import { LoadingProvider } from './context/LoadingContext';
-import { AuthProvider } from './contexts/AuthContext';
 import AppRouter from './router';
 
 function App() {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AuthProvider>
-        <LoadingProvider>
-          <AppRouter />
-        </LoadingProvider>
-      </AuthProvider>
-    </ThemeProvider>
-  );
+  return <AppRouter />;
 }
 
 export default App;
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { LoadingProvider } from './context/LoadingContext';
 import { AuthProvider } from './contexts/AuthContext';
+import { ThemeProvider } from './components/theme-provider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <LoadingProvider>
-        <App />
-      </LoadingProvider>
-    </AuthProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AuthProvider>
+        <LoadingProvider>
+          <App />
+        </LoadingProvider>
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
