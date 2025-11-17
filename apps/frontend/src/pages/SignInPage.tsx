f6c36e59e2d93379fa70a19538f9c42a32e9baf3
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2025-11-17 21:13:54 +0530

    update: Updated sign in page button color

--- apps/frontend/src/pages/SignInPage.tsx
+++ apps/frontend/src/pages/SignInPage.tsx
@@ -65,7 +65,7 @@ export default function SignInPage() {
               </>
             ) : (
               <>
-                <svg className="w-6 h-6" viewBox="0 0 24 24">
+                <svg className="w-6 h-6 text-white" viewBox="0 0 24 24">
                   <path
                     fill="currentColor"
                     d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
@@ -91,7 +91,7 @@ export default function SignInPage() {
                     clipRule="evenodd"
                   />
                 </svg>
-                <span>Continue with Google</span>
+                <span className='text-white'>Continue with Google</span>
               </>
             )}
           </Button>
